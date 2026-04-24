527804f8ff445f6faa65ca925aff9af36993d250
Rixvix <rixvix@protonmail.com>
2026-04-24 16:48:25 -0400

    revert: remove unnecessary callbackUrl (auto-detect works on Vercel)

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -3,7 +3,4 @@ import { uploadRouter } from "./core";
 
 export const { GET, POST } = createRouteHandler({
   router: uploadRouter,
-  config: {
-    callbackUrl: `${process.env.NEXT_PUBLIC_APP_URL}/api/uploadthing`,
-  },
 });
